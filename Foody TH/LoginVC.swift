278574f11d67190d0792e56995339db78150b99d
taeshawit <s.taeshawit@icloud.com>
2016-07-04 23:02:51 +0700

    Foody - First Commit
    Foody - First Commit

---
+++ Foody TH/LoginVC.swift
@@ -0,0 +1,35 @@
+//
+//  LoginVC.swift
+//  Foody TH
+//
+//  Created by Taeshawit Suwanwigo on 7/2/2559 BE.
+//  Copyright © 2559 Achiraya Klinpipat. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Foody TH/MainVC.swift
@@ -0,0 +1,29 @@
+//
+//  MainVC.swift
+//  Foody TH
+//
+//  Created by Taeshawit Suwanwigo on 7/2/2559 BE.
+//  Copyright © 2559 Achiraya Klinpipat. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func loginAction(sender: AnyObject) {
+        let vc = LoginVC()
+        self.presentViewController(vc, animated: true, completion: nil)
+    }
+}
